31f0edd738b987b57ef32ed57b04bbc102be0617
Shokri <shokre4@hotmail.com>
2022-04-05 21:41:50 +0400

    Initial Commit

---
+++ TempConversion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TempConversion
+//
+//  Created by Shokri Alnajjar on 05/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TempConversion/TempConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConversionApp.swift
+//  TempConversion
+//
+//  Created by Shokri Alnajjar on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TempConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
